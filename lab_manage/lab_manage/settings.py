b1dd8b3b7afe87c9831f12f4e501601f3daec4c6
“Jahauy” <edison0902@foxmail.com>
2022-11-03 23:37:49 +0800

    create user_infos app.

--- lab_manage/lab_manage/settings.py
+++ lab_manage/lab_manage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-glu(3_^j+%ph1dapj@0wrbe&rd2m-p*1bk%otpoggezc4#m$)$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # ------------------------------------
+    "user_infos",
 ]
 
 MIDDLEWARE = [
