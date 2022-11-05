25d068368faa6d15be114771180433d4413a8733
“Jahauy” <edison0902@foxmail.com>
2022-11-05 14:10:19 +0800

    1. 新增用户模型 2. 新增登录模板

--- lab_manage/user_infos/admin.py
+++ lab_manage/user_infos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+# 注册进管理员界面
+admin.site.register(models.User)
--- lab_manage/user_infos/models.py
+++ lab_manage/user_infos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    userid = models.CharField(verbose_name='用户id', max_length=32, unique=True)
+    name = models.CharField(verbose_name='用户名', max_length=32)
+    password = models.CharField(verbose_name='密码', max_length=64)
+    ctime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['ctime']
+        verbose_name = '用户'
+        verbose_name_plural = '用户'
