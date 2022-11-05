1468c0d2cb435d3c3f4464fb0b833b6ceaa91610
“Jahauy” <edison0902@foxmail.com>
2022-11-05 18:22:17 +0800

    补充登录界面

--- lab_manage/user_infos/urls.py
+++ lab_manage/user_infos/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'user_infos'
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('login/', views.login, name='login'),
-    # path('register/', views.register, name='register'),
+    path('register/', views.register, name='register'),
     # path('', views.logout, name='logout'),
 ]
--- lab_manage/user_infos/views.py
+++ lab_manage/user_infos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from .models import User
 
 # Create your views here.
 '''
@@ -10,19 +11,42 @@ from django.shortcuts import render, redirect, reverse
 '''
 
 
+# 输入参数的合法性校验，TODO
+def _review_inputs(userid, password):
+    print(f'[INFO] userid={userid}')
+    if userid is None or password is None:
+        return False
+    return True
+
+
 def login(request):
-    pass
+    if request.method == 'POST':
+        userid = request.POST.get('userid')
+        password = request.POST.get('password')
+        message = 'Please check input! '
+
+        if _review_inputs(userid.strip(), password):
+            try:
+                user = User.objects.get(userid=userid)
+                if user.password == password:
+                    return redirect(reverse('user_infos::index'))
+                else:
+                    message = 'Password invalid...'
+            except:
+                message = 'User not found...'
+
+        return render(request, 'user_infos/login.html', {'message': message, })
     return render(request, 'user_infos/login.html')
 
-#
-# def index(request):
-#     pass
-#     return render(request, '')
-#
-#
-# def register(request):
-#     pass
-#     return render(request, '')
+
+def index(request):
+    pass
+    return render(request, 'user_infos/index.html')
+
+
+def register(request):
+    pass
+    return render(request, 'user_infos/register.html')
 #
 #
 # def logout(request):
