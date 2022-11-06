23e71939671f7f712309a6d47e0fc3c258c17a10
“Jahauy” <edison0902@foxmail.com>
2022-11-07 00:52:51 +0800

    update

--- lab_manage/lab_manage/settings.py
+++ lab_manage/lab_manage/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-glu(3_^j+%ph1dapj@0wrbe&rd2m-p*1bk%otpoggezc4#m$)$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
